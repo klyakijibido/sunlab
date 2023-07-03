12c749466831a5f5566f0dda955c58ed928c8c71
Yaroslav Kuzhakhmetov <93444486+klyakijibido@users.noreply.github.com>
2023-07-04 02:23:27 +0300

    Add tovar_controller

---
+++ app/controllers/tovar_controller.rb
@@ -0,0 +1,14 @@
+class TovarsController < ApplicationController
+  # в нем три метода - три экшена
+  # GET /garments/1
+  def show
+  end
+
+  # GET /garments
+  def index
+  end
+
+  # GET /garments/current
+  def current
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  resources :tovars, only: %i[show index]
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+    # root "tovars#index"
 end
