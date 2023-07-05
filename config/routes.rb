761696a83f9cf920da686474d0180f139eda13a8
Yaroslav Kuzhakhmetov <93444486+klyakijibido@users.noreply.github.com>
2023-07-05 15:39:31 +0300

    return routes.rb to my view

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
 Rails.application.routes.draw do
-  resources :tovars, only: %i[show index] do
-    collection do
-      get :current
-    end
-  end
+  resources :tovars, only: %i[show index]
 
-    # root "tovars#index"
+  # root "tovars#index"
 end
